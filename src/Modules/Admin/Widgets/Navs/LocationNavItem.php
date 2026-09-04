c4187985ad5aa48a2cc5c920dcb5132eab55349e
davidhirtz <david.hirtz@me.com>
2026-09-04 09:34:08 +0200

    Menu update

--- src/Modules/Admin/Widgets/Navs/LocationNavItem.php
+++ src/Modules/Admin/Widgets/Navs/LocationNavItem.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace Hirtz\Location\Modules\Admin\Widgets\Navs;
 
-use Hirtz\Skeleton\I18n\Lang;
 use Hirtz\Location\Models\Location;
 use Hirtz\Location\Models\Tag;
 use Hirtz\Location\Modules\ModuleTrait;
+use Hirtz\Skeleton\I18n\Lang;
 use Hirtz\Skeleton\Widgets\Navs\NavItem;
 use Override;
-use Yii;
 
 class LocationNavItem extends NavItem
 {
@@ -20,10 +19,13 @@ class LocationNavItem extends NavItem
 
     public function __construct(array $config = [])
     {
-        $this->label ??= Lang::t('location', 'LOCATION_NAV_ITEM_PLACES');
-        $this->icon ??= 'map-marked-alt';
+        $this->label ??= Lang::t('location', 'COMMON_LOCATIONS');
+        $this->icon ??= 'map-marker-alt';
         $this->order ??= 50;
         $this->url ??= ['/admin/location/location/index'];
+
+        $this->roles([Location::AUTH_LOCATION_CREATE]);
+        $this->routes(['admin/location/location/', 'admin/location/location-tag/']);
 
         parent::__construct($config);
     }
@@ -41,28 +43,17 @@ class LocationNavItem extends NavItem
 
     protected function addSubnavItems(): void
     {
-        $this->addItem($this->getLocationIndexItem(), $this->getTagIndexItem());
-    }
-
-    protected function getLocationIndexItem(): ?NavItem
-    {
-        return NavItem::make()
-            ->icon('map-marker-alt')
-            ->label(Lang::t('location', 'COMMON_LOCATIONS'))
-            ->order(10)
-            ->url(['/admin/location/location/index'])
-            ->roles([Location::AUTH_LOCATION_CREATE])
-            ->routes(['admin/location/location/', 'admin/location/location-tag/']);
+        $this->addItem($this->getTagIndexItem());
     }
 
     protected function getTagIndexItem(): ?NavItem
     {
         return $this->showTags
             ? NavItem::make()
-                ->icon('tags')
+                //->icon('tags')
                 ->label(Lang::t('location', 'COMMON_TAGS'))
-                ->order(20)
                 ->url(['/admin/location/tag/index'])
+                ->order(20)
                 ->roles([Tag::AUTH_TAG_CREATE])
                 ->routes(['admin/location/tag'])
             : null;
